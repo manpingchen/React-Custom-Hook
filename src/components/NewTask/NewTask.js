ff753602634655c4d4d65e156826cf8a1b028980
Man Ping Chen <manpingchen@gmail.com>
2022-02-02 11:35:36 +0000

    implement useHook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,26 @@
-import { useState } from 'react';
-
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest } = useHttp();
 
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-movie-890b6-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  const createTaskHandler = (taskText, tasksObj) => {
+    const generatedId = tasksObj.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
+    props.onAddTask(createdTask);
+  };
 
-      const data = await response.json();
+  const enterTaskHandler = (taskText) => {
+    const requestConfig = {
+      url: "https://react-movie-890b6-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: { "Content-Type": "application/json" },
+    };
 
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendRequest(requestConfig, createTaskHandler.bind(null, taskText));
   };
 
   return (
